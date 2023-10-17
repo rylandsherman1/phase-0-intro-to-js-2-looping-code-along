6ec5d5bf1da7d7b385948ae3173d66031fd10758
Ryland Sherman <rylandsherman1@gmail.com>
2023-10-17 16:19:18 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, surprise) {
+    const messages = []
+    for (let i = 0; i < cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+        messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+
+    return messages;
+}
+
+writeCards(cards, "surprise");
+
+
+function countDown() {
+let countDown = 10;
+while (countDown >= 0) {
+console.log(countDown--);
+}
+}
